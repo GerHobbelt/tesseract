12f9e68cf1d47040a196a4fdf3eea89dd9732e05
Ger Hobbelt <ger@hobbelt.com>
2021-07-15 17:28:07 +0200

    overriding git's automatic merge decision: forcing the code to match changes in SHA-1: 8d1c1e1da262f5a148925e272248e4fd740a9e53
    * just a couple of 'shadowed local variables' compiler warning fixes that got through while I manually extracted the template work from my mainline (warnings due to running MSVC at Level 4)
    
    [sw]: Use different fix for blamer.cpp
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/ccstruct/blamer.cpp
+++ src/ccstruct/blamer.cpp
@@ -311,7 +311,6 @@ void BlamerBundle::SetChopperBlame(const WERD_RES *word, bool debug) {
   if (NoTruth() || !truth_has_char_boxes_ || word->chopped_word->blobs.empty()) {
     return;
   }
-  std::string debug_str;
   bool missing_chop = false;
   int num_blobs = word->chopped_word->blobs.size();
   int box_index = 0;
@@ -331,6 +330,7 @@ void BlamerBundle::SetChopperBlame(const WERD_RES *word, bool debug) {
     }
   }
   if (missing_chop || box_index < norm_truth_word_.length()) {
+    std::string debug_str;
     if (missing_chop) {
       debug_str += "Detected missing chop (tolerance=" + std::to_string(norm_box_tolerance_);
       debug_str += ") at Bounding Box=";
