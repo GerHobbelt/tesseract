413ff7d49126ae81db7f8998d369fced30b34411
Ger Hobbelt <ger@hobbelt.com>
2021-07-31 17:10:10 +0200

    optimization: fewer heap alloc/free operations when removing STL string concatenation, rolling all concatenations in the already present snprintf() call.

--- src/ccutil/unicharset.cpp
+++ src/ccutil/unicharset.cpp
@@ -1061,14 +1061,11 @@ std::string CHAR_FRAGMENT::to_string(const char *unichar, int pos, int total, bo
   if (total == 1) {
     return std::string(unichar);
   }
-  std::string result;
-  result += kSeparator;
-  result += unichar;
-  char buffer[kMaxLen];
-  snprintf(buffer, kMaxLen, "%c%d%c%d", kSeparator, pos, natural ? kNaturalFlag : kSeparator,
+  char buffer[kMaxLen + 8];
+  snprintf(buffer, kMaxLen, "%c%s%c%d%c%d", kSeparator, unichar, kSeparator, pos, natural ? kNaturalFlag : kSeparator,
            total);
-  result += buffer;
-  return result;
+  assert(strlen(buffer) < sizeof(buffer));
+  return buffer;
 }
 
 CHAR_FRAGMENT *CHAR_FRAGMENT::parse_from_string(const char *string) {
