ae08dd97c13c5b9eb365052868d2a1060c673da5
JKamlah <jan.kamlah@gmail.com>
2022-04-04 17:41:40 +0200

    Add: Non-linear grayscale normalization as preprocessing based on nlbin (Thomas Breuel).

--- src/ccmain/tesseractclass.h
+++ src/ccmain/tesseractclass.h
@@ -757,6 +757,7 @@ public:
   BOOL_VAR_H(tessedit_dump_pageseg_images);
   // TODO: remove deprecated tessedit_do_invert in release 6.
   BOOL_VAR_H(tessedit_do_invert);
+  BOOL_VAR_H(normalize_grayscale);
   double_VAR_H(invert_threshold);
   INT_VAR_H(tessedit_pageseg_mode);
   INT_VAR_H(thresholding_method);
