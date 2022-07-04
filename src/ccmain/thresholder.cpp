1bd9f98c3ce07deba8b452fa7eae5c3337705591
JKamlah <jan.kamlah@gmail.com>
2022-07-04 15:35:19 +0200

    Fix error warning text, delete empty lines and old parameter config.

--- src/ccmain/thresholder.cpp
+++ src/ccmain/thresholder.cpp
@@ -276,7 +276,6 @@ Pix *ImageThresholder::pixNLNorm(Pix *pixs, int *pthresh) {
   return pixg;
 }
 
-
 /*----------------------------------------------------------------------*
  *                          Thresholding                                *
  *----------------------------------------------------------------------*/
@@ -489,7 +488,6 @@ Image ImageThresholder::GetPixNormRectGrey() {
   pix.destroy();
   return result;
 }
-
 
 // Otsu thresholds the rectangle, taking the rectangle from *this.
 void ImageThresholder::OtsuThresholdRectToPix(Image src_pix, Image *out_pix) const {
