6de27ae2b95d8ac1f5c0b1645a924ee9e86180b1
Ger Hobbelt <ger@hobbelt.com>
2023-08-21 02:05:31 +0200

    fix commit 24f16e3ed3337e495578adeec704839eeb41b093 which was missing this edit. --> fix memory mgt bugs in the pix display/debug code: GetPix() MUST always return a PIX that MUST be destroyed, no exceptions! Otherwise you get spurious errors as the code isn't yet smart enough to cope with such copy/link-only optimizations...

--- src/ccstruct/imagedata.cpp
+++ src/ccstruct/imagedata.cpp
@@ -196,15 +196,9 @@ void ImageData::SetPix(Image pix) {
 // Returns the Pix image for *this. Must be pixDestroyed after use.
 Image ImageData::GetPix() const {
 #ifdef TESSERACT_IMAGEDATA_AS_PIX
-#  ifdef GRAPHICS_DISABLED
-  /* The only caller of this is the scaling functions to prescale the
-   * source. Thus we can just return a new pointer to the same data. */
-  return internal_pix_.clone();
-#  else
   /* pixCopy always does an actual copy, so the caller can modify the
    * changed data. */
   return internal_pix_.copy();
-#  endif
 #else
   return GetPixInternal(image_data_);
 #endif
