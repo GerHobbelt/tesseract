9590515e3419b87339810e6b4b6e66b36f901002
Ger Hobbelt <ger@hobbelt.com>
2024-07-03 19:02:00 +0200

    PrintDetectorState() constructs a table in all but name for its diagnostic output; changing the column separator from SPACE to TAB both enables a slightly better read on terminals (stdio) and above all facilitates the new table recognizer that's part of the HTML log output formatter: the new table is now recognized by its heuristics and neatly rendered in `<table>...</table>` form.

--- src/ccmain/paragraphs.cpp
+++ src/ccmain/paragraphs.cpp
@@ -177,7 +177,7 @@ static void PrintDetectorState(const ParagraphTheory &theory,
     rows[i].AppendDebugInfo(theory, row);
     row.push_back(RtlEmbed(ri.text, !ri.ltr));
   }
-  PrintTable(output, " ");
+  PrintTable(output, "\t");
 
   tprintDebug("Active Paragraph Models:\n");
   unsigned m = 0;
