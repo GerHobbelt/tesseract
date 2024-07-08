a173edb19dda9dbb56704d70e0019adad194893a
Ger Hobbelt <ger@hobbelt.com>
2024-07-09 01:02:15 +0200

    making sure the DISABLED_LEGACY_ENGINE works everywhere the same. MSVC warning fixed.

--- src/ccmain/applybox.cpp
+++ src/ccmain/applybox.cpp
@@ -16,6 +16,10 @@
  *
  **********************************************************************/
 
+#ifdef HAVE_TESSERACT_CONFIG_H
+#  include "config_auto.h"
+#endif
+
 #if !DISABLED_LEGACY_ENGINE
 #  include <leptonica/allheaders.h>
 #  include <cctype>
