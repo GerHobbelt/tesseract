eea578a19534dcbf265f01f3f1284254183b563a
Ger Hobbelt <ger@hobbelt.com>
2024-07-15 16:31:16 +0200

    improved parameter description
    # Conflicts:
    #	src/ccmain/tesseractclass.cpp

--- src/ccmain/tesseractclass.cpp
+++ src/ccmain/tesseractclass.cpp
@@ -117,9 +117,9 @@ Tesseract::Tesseract()
                     "For standard Otsu use 0.0, otherwise 0.1 is recommended",
                     this->params())
     , INT_INIT_MEMBER(tessedit_ocr_engine_mode, tesseract::OEM_DEFAULT,
-                      "Which OCR engine(s) to run (Tesseract, LSTM, both)."
-                      " Defaults to loading and running the most accurate"
-                      " available.",
+                      "Which OCR engine(s) to run (0: Tesseract, 1: LSTM, 2: both, 3: default). "
+                      "Defaults to loading and running the most accurate "
+                      "available.",
                       this->params())
     , STRING_MEMBER(tessedit_char_blacklist, "", "Blacklist of chars not to recognize",
                     this->params())
