a993c3432f3c464c36f62600266a1e540627184b
Ger Hobbelt <ger@hobbelt.com>
2024-09-24 22:17:15 +0200

    typo/language fix in flag description

--- src/training/lstmeval.cpp
+++ src/training/lstmeval.cpp
@@ -32,7 +32,7 @@ STRING_VAR(lstmeval_traineddata, "",
                          "be the traineddata file that was given to the trainer");
 STRING_VAR(lstmeval_eval_listfile, "", "File listing sample files in lstmf training format.");
 INT_VAR(lstmeval_max_image_MB, 2000, "Max memory to use for images.");
-INT_VAR(lstmeval_verbosity, 1, "Amount of diagnosting information to output (0-2).");
+INT_VAR(lstmeval_verbosity, 1, "Amount of diagnostics information to output (0-2).");
 
 FZ_HEAPDBG_TRACKER_SECTION_END_MARKER(_)
 
